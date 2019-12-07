cac2f8302dbd27604f4089257f7bd8d7d48e4694
Saumya9896 <50162668+Saumya9896@users.noreply.github.com>
2019-12-08 02:06:11 +0530

    add initial instruments website files

---
+++ index.js
@@ -0,0 +1,94 @@
+//PIANO
+document.querySelector(".r").addEventListener("click",function(){
+   var drum_r = new Audio("piano/r.mp3");
+   drum_r.play();
+})
+
+document.querySelector(".t").addEventListener("click",function(){
+   var drum_t = new Audio("piano/t.mp3");
+   drum_t.play();
+})
+document.querySelector(".y").addEventListener("click",function(){
+   var drum_y = new Audio("piano/y.mp3");
+   drum_y.play();
+})
+document.querySelector(".u").addEventListener("click",function(){
+   var drum_u = new Audio("piano/u.mp3");
+   drum_u.play();
+
+})
+document.querySelector(".i").addEventListener("click",function(){
+   var drum_i = new Audio("piano/i.mp3");
+   drum_i.play();
+})
+document.querySelector(".o").addEventListener("click",function(){
+   var drum_o = new Audio("piano/o.mp3");
+   drum_o.play();
+})
+document.querySelector(".p").addEventListener("click",function(){
+   var drum_p = new Audio("piano/p.mp3");
+   drum_p.play();
+})
+
+//GUITAR 
+
+document.querySelector(".a").addEventListener("click",function(){
+   var drum_a = new Audio("guitar/a.mp3");
+   drum_a.play();
+})
+
+document.querySelector(".s").addEventListener("click",function(){
+   var drum_s = new Audio("guitar/s.mp3");
+   drum_s.play();
+})
+document.querySelector(".d").addEventListener("click",function(){
+   var drum_d = new Audio("guitar/d.mp3");
+   drum_d.play();
+})
+document.querySelector(".f").addEventListener("click",function(){
+   var drum_f = new Audio("guitar/f.mp3");
+   drum_f.play();
+})
+document.querySelector(".g").addEventListener("click",function(){
+   var drum_g = new Audio("guitar/g.mp3");
+   drum_g.play();
+})
+document.querySelector(".h").addEventListener("click",function(){
+   var drum_h = new Audio("guitar/h.mp3");
+   drum_h.play();
+})
+
+
+//DRUMS
+document.querySelector(".z").addEventListener("click",function(){
+   var drum_w = new Audio("drum/crash.mp3");
+   drum_w.play();
+ 
+})
+
+document.querySelector(".x").addEventListener("click",function(){
+   var drum_x = new Audio("drum/kick-bass.mp3");
+   drum_x.play();
+})
+document.querySelector(".c").addEventListener("click",function(){
+   var drum_c = new Audio("drum/snare.mp3");
+   drum_c.play();
+})
+document.querySelector(".v").addEventListener("click",function(){
+   var drum_v = new Audio("drum/tom-1.mp3");
+   drum_v.play();
+})
+document.querySelector(".b").addEventListener("click",function(){
+   var drum_b = new Audio("drum/tom-2.mp3");
+   drum_b.play();
+})
+document.querySelector(".n").addEventListener("click",function(){
+   var drum_n = new Audio("drum/tom-3.mp3");
+   drum_n.play();
+})
+document.querySelector(".m").addEventListener("click",function(){
+   var drum_m = new Audio("drum/tom-4.mp3");
+   drum_m.play();
+})
+
+
